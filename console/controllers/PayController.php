f7ae86a3ddb8fdbe7a4c6b793b38778ebcfcdaf3
sanya <sanyapodolski@gmail.com>
2021-01-17 18:03:03 +0200

    правки с кроном

--- console/controllers/PayController.php
+++ console/controllers/PayController.php
@@ -18,7 +18,7 @@ class PayController extends Controller
 
             foreach ($posts as $post){
 
-                $user = User::find()->where(['id' => $post['user_id']])->one();
+                $user = User::find()->where(['id' => $post['user_id']])->orWhere(['old_id' => $post['old_user_id']])->one();
 
                 $tarif = Tarif::find()->where(['value' => $post['tarif_id']])->asArray()->one();
 
