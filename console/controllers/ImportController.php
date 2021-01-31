9ed93debad56ffec403bd02e813c94b709b6e710
sanya <sanyapodolski@gmail.com>
2021-01-31 11:37:20 +0200

    импорт казань

--- console/controllers/ImportController.php
+++ console/controllers/ImportController.php
@@ -33,8 +33,8 @@ use yii\console\Controller;
 class ImportController extends Controller
 {
 
-    public $tablePref = 'himki';
-    public $city_id = 3;
+    public $tablePref = 'kazany';
+    public $city_id = 9;
 
     public function actionUser()
     {
@@ -265,7 +265,7 @@ class ImportController extends Controller
 
             }
 
-            $userComfort = \Yii::$app->db2->createCommand("SELECT * FROM `user_comfort` WHERE `post_id` = {$post['id']} and `city_id` = '3'")->queryAll();
+            $userComfort = \Yii::$app->db2->createCommand("SELECT * FROM `user_comfort` WHERE `post_id` = {$post['id']} and `city_id` = '11'")->queryAll();
 
             if ($userComfort) foreach ($userComfort as $userComfortItem){
 
