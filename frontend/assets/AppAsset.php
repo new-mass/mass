3e65bafaf2dfbe6583b70941ab85480350c4a806
sanya <sanyapodolski@gmail.com>
2021-03-14 10:08:35 +0200

    добавлены метки

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=5',
+        'css/site.css?v=6',
     ];
     public $js = [
     ];
