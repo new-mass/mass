1cd7737cbdcc5491715f65d4897bea3ffd65de5c
sanya <sanyapodolski@gmail.com>
2021-04-14 18:56:14 +0300

    правки с сортировкой анкет

--- console/controllers/PayController.php
+++ console/controllers/PayController.php
@@ -52,6 +52,7 @@ class PayController extends Controller
                     }else{
 
                         $post['pay_time'] = time() + 3600;
+                        $post['sorting'] = rand(0, 50);
 
                         $post->save();
 
