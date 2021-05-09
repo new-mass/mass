385afc1e32fd5647949cff85ad6dbca7dbb50a44
sanya <sanyapodolski@gmail.com>
2021-05-09 12:18:53 +0300

    правка с карточкой

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=8',
+        'css/site.css?v=9',
     ];
     public $js = [
     ];
