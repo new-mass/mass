ba60d42ac784e386fcccbd67f8d804451e458a5e
sanya <sanyapodolski@gmail.com>
2021-10-17 17:07:52 +0300

    правки с мета тегами

--- frontend/components/MetaTagsHelper.php
+++ frontend/components/MetaTagsHelper.php
@@ -14,17 +14,49 @@ class MetaTagsHelper
         else $pol = 'Массажистка';
 
         return $pol . ' ' . $post['name'] . ' возраст ' . $post['age'] . ' цена за сеанс ' .
-            $post['price'] . ' рублей, город ' . $city['value'] . ' номер телефона '.$post['phone'] .' ID '.$post['id'];
+            $post['price'] . ' рублей, город ' . $city['value'] . ' номер телефона '.$post['phone'];
 
     }
     public static function singleDes($post, $city){
 
+        $pol = '';
+
         if ($post['pol'] == 2) $pol = 'Массажистка ';
         else $pol = 'Массажистка';
 
-        return $pol . ' ' . $post['name'] . ' работаю в городе ' . $city['value'] . ' , 
-        стоимость сеанса составляет ' . $post['price'] . ' рублей, жду Ваших звонков ID '.$post['id'] ;
+        if (iconv_strlen($post['about']) < 100) {
 
+            return $pol . ' ' . $post['name'] . ' работаю в городе ' . $city['value'] . ' , стоимость сеанса составляет '
+                . $post['price'] . ' рублей, жду Ваших звонков ID '.$post['id'] ;
+
+        } else {
+
+            $params = explode('.',$post['about']);
+
+            $count = 0;
+
+            $string = array();
+
+            foreach ($params as $value){
+
+                $count = $count + iconv_strlen($value);
+
+                if ($value != NULL){
+
+                    $string[] = $value;
+
+                }
+
+                if ($count > 200){
+
+                    return implode('.', $string);
+
+                }
+
+            }
+            return implode('.', $string).' ID '.$post['id'];
+
+        }
     }
 
 
