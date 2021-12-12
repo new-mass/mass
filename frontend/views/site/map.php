74629f2f00361b7a8ac4c5b1cbb1e00469306288
sanya <sanyapodolski@gmail.com>
2021-12-12 11:42:45 +0200

    изменена дата

--- frontend/views/site/map.php
+++ frontend/views/site/map.php
@@ -7,65 +7,66 @@
 <?php /* @var $intimHair array */ ?>
 <?php /* @var $massgDlya array */ ?>
 <?php /* @var $posts array */ ?>
+//год месяц день
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <?php echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'; ?>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?></loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>1</priority>
 </url>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?>/age_ot-18-do-20-let</loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>0.9</priority>
 </url>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?>/age_ot-21-do-25-let</loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>0.9</priority>
 </url>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?>/age_ot-31-do-40-let</loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>0.9</priority>
 </url>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?>/age_ot-40-do-50-let</loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>0.9</priority>
 </url>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?>/age_starshe-51-goda</loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>0.9</priority>
 </url>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?>/price_ot-3000</loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>0.9</priority>
 </url>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?>/price_ot-2000-do-3000</loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>0.9</priority>
 </url>
 <url>
     <loc>https://<?php echo Yii::$app->request->serverName?>/price_do-2000</loc>
-    <lastmod>2021-11-22</lastmod>
+    <lastmod>2021-12-12</lastmod>
     <priority>0.9</priority>
 </url>
 
 <?php if ($metro) foreach ($metro as $metroItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/metro_<?php echo $metroItem['url']?></loc>
-        <lastmod>2021-11-22</lastmod>
+        <lastmod>2021-12-12</lastmod>
         <priority>0.9</priority>
     </url>
 <?php endforeach; ?>
 <?php if ($massgDlya) foreach ($massgDlya as $massgDlyaItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/massazh-dlya_<?php echo $massgDlyaItem['url']?></loc>
-        <lastmod>2021-11-22</lastmod>
+        <lastmod>2021-12-12</lastmod>
         <priority>0.9</priority>
     </url>
 <?php endforeach; ?>
@@ -73,7 +74,7 @@
 <?php if ($rayon) foreach ($rayon as $rayonItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/rayon_<?php echo $rayonItem['url']?></loc>
-        <lastmod>2021-11-22</lastmod>
+        <lastmod>2021-12-12</lastmod>
         <priority>0.9</priority>
     </url>
 <?php endforeach; ?>
@@ -81,7 +82,7 @@
 <?php if ($service) foreach ($service as $serviceItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/service_<?php echo $serviceItem['url']?></loc>
-        <lastmod>2021-11-22</lastmod>
+        <lastmod>2021-12-12</lastmod>
         <priority>0.9</priority>
     </url>
 <?php endforeach; ?>
@@ -89,7 +90,7 @@
 <?php if ($place) foreach ($place as $placeItem) : ?>
     <url>
         <loc>https://<?php echo Yii::$app->request->serverName?>/place_<?php echo $placeItem['url']?></loc>
-        <lastmod>2021-11-22</lastmod>
+        <lastmod>2021-12-12</lastmod>
         <priority>0.9</priority>
     </url>
 <?php endforeach; ?>
