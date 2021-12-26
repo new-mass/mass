88952f750b2fed0bb608f2a137d2cec3f358caed
sanya <sanyapodolski@gmail.com>
2021-12-26 14:40:43 +0200

    яндекс карта для метро спб правки

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=29',
+        'css/site.css?v=30',
     ];
     public $js = [
         '/js/script.js?v=22',
