9557edb669fca9ec709d705ef461e4b85d613124
sanya <sanyapodolski@gmail.com>
2021-12-30 20:18:24 +0200

    правки с валидностью верстки

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=30',
+        'css/site.css?v=31',
     ];
     public $js = [
         '/js/script.js?v=22',
