2b6111b4e71e6f77ee71ded9ccb27a470995fdd3
sanya <sanyapodolski@gmail.com>
2022-01-17 18:21:49 +0200

    новая касса

--- common/components/PayLinkBuilder.php
+++ common/components/PayLinkBuilder.php
@@ -10,13 +10,18 @@ class PayLinkBuilder
     {
         $order_id = $userId.'_'.$city;
 
-        $sign = \md5(Yii::$app->params['merchant_id'].':'.$paySum.':'.Yii::$app->params['fk_merchant_key'].':'.$order_id);
+        $merchant_id = Yii::$app->params['freekassa_id'];
+        $secret_word = Yii::$app->params['freekassa_sercret_word'];
+        $order_amount = $paySum;
+        $currency = 'RUB';
+        $sign = md5($merchant_id.':'.$order_amount.':'.$secret_word.':'.$currency.':'.$order_id);
 
-        $params = 'm='.Yii::$app->params['merchant_id'].
+        $params = '?m='.Yii::$app->params['freekassa_id'].
             '&oa='.$paySum.
             '&o='.$order_id.
+            '&currency=RUB'.
             '&s='.$sign;
 
-        return Yii::$app->params['cassa_url'].$params;
+        return Yii::$app->params['new_cassa_url'].$params;
     }
 }
