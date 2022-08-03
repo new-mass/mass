d05b8dfad8fffe2dac2aef6ea4911dd6b9f0a03c
Your Name <you@example.com>
2022-08-03 18:25:31 +0300

    правки

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=38',
+        'css/site.css?v=39',
     ];
     public $js = [
         '/js/script.js?v=28',
