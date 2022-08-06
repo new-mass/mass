963627dd35274ac21e19725b67481769a4238521
Your Name <you@example.com>
2022-08-06 09:27:40 +0300

    edit

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=40',
+        'css/site.css?v=41',
     ];
     public $js = [
         '/js/script.js?v=28',
